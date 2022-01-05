daea12860de5cd198c07d08b7ec049e0b1f50aa5
SDBoahen <SDBoahen@gmail.com>
2022-01-05 16:58:53 -0500

    1-5-2022 - Sam - We Established Our Main Branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// Hook Imports
 import { useEffect, useState } from 'react';
 
+// Component Imports
 import UserCard from './UserCard';
 
 
@@ -11,61 +14,74 @@ import UserCard from './UserCard';
 function App() {
 
 
-  const [ users, setUsers ] = useState( [] )
-  console.log("State Of Our  users ->", users)  //
+  //// State
+    const [ users, setUsers ] = useState( [] )
+    console.log("State Of Our  users ->", users)
+  //// State
 
 
-  useEffect( 
-    
-    ()=>{
+  //// Global Constants
+    const USERS_URL = "http://localhost:9292/users"
+  //// Global Constants
 
-      fetch("http://localhost:9292/users")
-      .then( r => r.json() )
-      .then( 
 
-        (fetchedUserArrayOfObjs)=>{ console.log("fetchedUserArrayOfObjs: ", fetchedUserArrayOfObjs) 
+  const usersGetFetch=()=>{
 
-          setUsers( [ ...fetchedUserArrayOfObjs ]  )
+    fetch(USERS_URL)
+    .then(r => r.json())
+    .then( handleUsersGetFetchResponse )
 
-        }
-        
-      )
+  }
+  const handleUsersGetFetchResponse=(fetchedUserArrayOfObjs)=>{ 
+    console.log("fetchedUserArrayOfObjs: ", fetchedUserArrayOfObjs)
+      // So That We Can See The  reponse  That We Got Back
+      // - Sam 
+    setUsers( [ ...fetchedUserArrayOfObjs ]  )
+  }
 
-    }
+
+  useEffect( usersGetFetch , [] )
+
   
-  , [] )
-
+  const renderUserCardsMapCallback=(eachUser)=>{
+    return(<UserCard key={eachUser.id} userProp={eachUser} />)
+  }
+  const mappedUsers = users.map( renderUserCardsMapCallback ) 
 
   return (
     <div className="App">
       <header className="App-header">
-        
         <img src={logo} className="App-logo" alt="logo" />
-
         <h1>Hello User's From The Backend! 🙌 :)</h1>
-        {
-
-          users.map( 
-
-            (eachUser)=>{
-
-              return( <UserCard 
-                
-                key={eachUser.id} // For React  🤖
-                userProp={eachUser} // For Us  :)
-                
-              /> )
-
-            }
-
-           )
-
-        }
-
-
+        {mappedUsers}
       </header>
     </div>
   );
-}
 
+}
 export default App;
+
+
+
+
+
+  // users:
+  //// let numUsers = users.length
+
+    // case: 0
+      // code for case 0
+      // time: 0+  ~  1s
+    // case: 1
+      // code for case 1
+      // time: time * 2
+    // case: 1 + numUsers
+      // code for case (1 + n)
+      // time: time * numUsers
+
+    // case: 100 + numUsers
+      // code for case (1 + n)
+      // time: (time * 100users) + newTime * numUsers
+
+  //// n = 10
+
+  //renderUsers - function
