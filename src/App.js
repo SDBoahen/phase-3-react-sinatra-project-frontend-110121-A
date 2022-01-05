db9e162571de5bfc0faf2403186a421bda459f35
SDBoahen <SDBoahen@gmail.com>
2022-01-05 15:45:30 -0500

    1-5-2022 - Removed that dang pTag

--- src/App.js
+++ src/App.js
@@ -39,11 +39,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
 
         <h1>Hello User's From The Backend! 🙌 :)</h1>
         {
